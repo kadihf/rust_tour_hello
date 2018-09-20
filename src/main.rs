ef1e2e87ccaa8b2d0e8bda205a7bfd0a1db28c5f
kadihf <kadihfosahfuu@gmail.com>
2018-09-20 22:32:19 +0900

    add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,23 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 
 
 fn main() {
+    let mut numbers = Vec::new();
 
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("erro parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd Number ...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
 
 #[test]
